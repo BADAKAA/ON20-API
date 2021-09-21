7c6c01a3d467d626f74a6656e8e7251d1e3c7484
LowisSpross <spross@querformat.info>
2021-09-21 16:10:04 +0200

    added events

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(express.json())
 const fs = require("fs");
 const data = require("./database.json");
 const ufos = require("./ufo-data.json");
-
+//
+const events = require("./event-data.json");
+//
 
 app.listen(
     PORT,
@@ -19,8 +21,109 @@ app.get("/data", async (req, res) => {
     return res.status(200).send(data)
 })
 
+app.get("/test", (req, res) => {
+    return res.status(223).send({ message: "Data"});
+})
 
-app.post("/data/:name", (req, res) => {
+app.post("/banana", (req, res) => {
+    
+    const object = req.body;
+    console.log(object);
+
+    return res.status(200).send();
+})
+
+///EVENT DATA BASE BUSINESS
+app.get("/events", async (req, res) => {
+    if (events) return res.status(200).send(events)
+    res.status(400).send("This data could not be found.");
+})
+
+//Endpunkt 1
+app.get("/events/:index", (req, res) => {
+    //return res.status(200).send(events);
+    if (!events) return res.status(404).send("Events data could not be found.");
+    const { index } = req.params;
+    if (!index) return res.status(200).send(events);
+    if (!events[index])
+      return res.status(404).send("A event with: " + index + " does not exist.");
+    return res.status(200).send(events[index]);
+  });
+  
+  //Endpunkt 2
+  app.post("/events/test", (req, res) => {
+    if (!events)
+      return res.status(404).send({ message: "Source data not found." });
+  
+    const { index } = req.params;
+    const { title } = req.body;
+    const { date } = req.body;
+    const { start } = req.body;
+    const { end } = req.body;
+    const { city } = req.body;
+    const { country } = req.body;
+    const { location } = req.body;
+    const { adress } = req.body;
+    const { description } = req.body;
+    const { image } = req.body;
+  
+    //Fehlerbehandlung die prüft, ob alle geforderten Attribute mitgegeben werden
+    if (
+      !(title &&
+      date &&
+      start &&
+      end &&
+      city &&
+      country &&
+      location &&
+      adress &&
+      description &&
+      image)
+    )
+      return res.status(404).send({ message: "Please specify the event" });
+    //Fehlerbehandlung 2 die prüft, ob ein Event mit dem Index bereits vorhanden ist
+    const duplicatedTitle = events.find((e) => e.title.trim() == title.trim());
+    if (duplicatedTitle)
+      return res
+        .status(409)
+        .send({ message: "An item with this title already exists." });
+    
+    events.push({
+      title: title,
+      date: date,
+      start: start,
+      end: end,
+      city: city,
+      country: country,
+      location: location,
+      adress: adress,
+      description: description,
+      image: image,
+    });
+    const eventsToBeSaved = JSON.stringify(events, null, 2);
+    fs.writeFile("./event-data.json", eventsToBeSaved, () =>
+      console.log("Data written to file.")
+    );
+    res.status(200).send({
+      message: `The item '${title}' was successfully added.`,
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+///
+
+
+/*app.post("/data/:name", (req, res) => {
     if (!data) return res.status(404).send({ message: "Source data not found." })
 
     const { name } = req.params;
@@ -40,7 +143,7 @@ app.post("/data/:name", (req, res) => {
         message: `The item '${name}' with content: '${content}' was successfully added.`
     })
 })
-
+*/
 
 
 
@@ -57,9 +160,6 @@ app.get("/ufo/:index", async (req, res) => {
     return res.status(200).send(ufos[index])
 })
 
-app.get("/events", async (req, res) => {
-    if (data) return res.status(200).send(data)
-    res.status(400).send("This data could not be found.");
-})
+
 
 
